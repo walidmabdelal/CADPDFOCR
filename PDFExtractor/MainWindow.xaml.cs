54783b49ef74707db1de18bae8161f813de8d193
walidmabdelal <walidmabdelal@gmail.com>
2021-01-20 09:07:44 +0400

    Add project files.

---
+++ PDFExtractor/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PDFExtractor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        private const string app_name = "PDF Miner v1.0";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = app_name;
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void statusBar_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
